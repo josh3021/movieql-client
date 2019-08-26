224662e1e2ef68e0972362748996de5ff4595b53
josh3021 <joseonghwan3021@gmail.com>
2019-08-26 11:04:19 +0900

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { HashRouter as Router, Route } from "react-router-dom";
+import client from "./apolloClient";
+
+import Home from "./Home";
+import Detail from "./Detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <React.Fragment>
+          <Route exact path={"/"} component={Home} />
+          <Route path={"/details/:movieId"} component={Detail} />
+        </React.Fragment>
+      </Router>
+    </ApolloProvider>
   );
 }
 
