1425187b7e280517077b59d4fc3cd96136b9b6ba
josh3021 <joseonghwan3021@gmail.com>
2019-08-26 20:34:03 +0900

    Aug 26th 2019 last commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from "react";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { HashRouter as Router, Route } from "react-router-dom";
-import client from "./apolloClient";
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import client from './apolloClient';
 
-import Home from "./Home";
-import Detail from "./Detail";
+import styled, {
+  createGlobalStyle,
+  css,
+  ThemeProvider
+} from 'styled-components';
+
+import Home from './Home';
+import Detail from './Detail';
+
+import theme from './theme';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <React.Fragment>
-          <Route exact path={"/"} component={Home} />
-          <Route path={"/details/:movieId"} component={Detail} />
-        </React.Fragment>
-      </Router>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Router>
+            <React.Fragment>
+              <Route exact path={'/'} component={Home} />
+              <Route path={'/details/:movieId'} component={Detail} />
+            </React.Fragment>
+          </Router>
+        </Container>
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: white;
+`;
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,47 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import { HOME_PAGE } from './queries';
 
-const Home = () => (
-  <Query query={HOME_PAGE}>
-    {({ loading, data, error }) => {
-      if (loading) return <h2>loading...</h2>;
-      else if (error) return <h1>umm.. Error Occured</h1>;
+import styled, { css, ThemeProvider } from 'styled-components';
 
-      if (data) {
-        console.log(data);
-        return data.movies.map(movie => (
-          <div key={movie.id}>
-            <h2>{movie.title}</h2>
-            {movie.genres.map((genre, index) => (
-              <h6 key={index}>{genre}</h6>
-            ))}
-            {movie.rating}
-          </div>
-        ));
-      }
-    }}
-  </Query>
+const Home = () => (
+  <Wrapper>
+    <Query query={HOME_PAGE}>
+      {({ loading, data, error }) => {
+        if (loading) return <h2>loading...</h2>;
+        else if (error) return <h1>umm.. Error Occured</h1>;
+
+        if (data) {
+          return data.movies.map(movie => (
+            <Card key={movie.id}>
+              <img
+                src={movie.medium_cover_image}
+                width={200}
+                height={300}
+                alt={movie.title}
+              />
+              <h2>{movie.title}</h2>
+              {movie.genres.map((genre, index) => (
+                <h6 key={index}>{genre}</h6>
+              ))}
+              {movie.rating}
+            </Card>
+          ));
+        }
+      }}
+    </Query>
+  </Wrapper>
 );
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-gap: 2.5%;
+  grid-template: repeat(5, 1fr) / repeat(4, 1fr);
+  grid-auto-flow: row;
+  padding: 3%;
+`;
+
+const Card = styled.div`
+  background-color: silver;
+`;
 
 export default Home;
